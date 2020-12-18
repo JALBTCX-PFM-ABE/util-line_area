fe0246d35e15a43ea782baf89c7b36a053136e71
Russ Johnson <russ.johnson@optimalgeo.com>
2020-12-18 15:30:37 -0600

    Initial version of files from release V7.0.0.0

---
+++ version.h
@@ -0,0 +1,98 @@
+
+/*********************************************************************************************
+
+    This is public domain software that was developed by or for the U.S. Naval Oceanographic
+    Office and/or the U.S. Army Corps of Engineers.
+
+    This is a work of the U.S. Government. In accordance with 17 USC 105, copyright protection
+    is not available for any work of the U.S. Government.
+
+    Neither the United States Government, nor any employees of the United States Government,
+    nor the author, makes any warranty, express or implied, without even the implied warranty
+    of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, or assumes any liability or
+    responsibility for the accuracy, completeness, or usefulness of any information,
+    apparatus, product, or process disclosed, or represents that its use would not infringe
+    privately-owned rights. Reference herein to any specific commercial products, process,
+    or service by trade name, trademark, manufacturer, or otherwise, does not necessarily
+    constitute or imply its endorsement, recommendation, or favoring by the United States
+    Government. The views and opinions of authors expressed herein do not necessarily state
+    or reflect those of the United States Government, and shall not be used for advertising
+    or product endorsement purposes.
+
+*********************************************************************************************/
+
+
+/*********************************************************************************************
+
+    This program is public domain software that was developed by 
+    the U.S. Naval Oceanographic Office.
+
+    This is a work of the US Government. In accordance with 17 USC 105,
+    copyright protection is not available for any work of the US Government.
+
+    This software is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+*********************************************************************************************/
+
+#ifndef VERSION
+
+#define     VERSION     "PFM Software - line_area V1.06 - 07/23/14"
+
+#endif
+
+/*
+
+    Version 1.0
+    Jan C. Depner
+    10/25/04
+
+    First version.
+
+
+    Version 1.01
+    Jan C. Depner
+    10/22/07
+
+    Added fflush calls after prints to stderr since flush is not automatic in Windows.
+
+
+    Version 1.02
+    Jan C. Depner
+    04/07/08
+
+    Replaced single .h files from utility library with include of nvutility.h
+
+
+    Version 1.03
+    Jan C. Depner
+    07/29/09
+
+    Changed %lf in printf statements to %f.  The MinGW folks broke this even though it works on every
+    compiler known to man (including theirs up to the last version).  No compiler warnings or errors
+    are issued.  Many thanks to the MinGW folks for fixing what wasn't broken.
+
+
+    Version 1.04
+    Jan C. Depner
+    04/29/10
+
+    Fixed posfix and fixpos calls to use numeric constants instead of strings for type.
+
+
+    Version 1.05
+    Jan C. Depner
+    05/06/11
+
+    Fixed problem with getopt that only happens on Windows.
+
+
+    Version 1.06
+    Jan C. Depner (PFM Software)
+    07/23/14
+
+    - Switched from using the old NV_INT64 and NV_U_INT32 type definitions to the C99 standard stdint.h and
+      inttypes.h sized data types (e.g. int64_t and uint32_t).
+
+*/
